0382cdc1c532f1cf81ccec6c3fe5d1a1bc1bb324
편정웅_1237297 <like5518@naver.com>
2025-02-02 19:20:16 +0900

    로그인 관련기능 토큰 오류 해결 전까지 주석처리.

--- Sub_PJT_1/front_temp/codeMaestro/src/router/ProtectedRoute.jsx
+++ Sub_PJT_1/front_temp/codeMaestro/src/router/ProtectedRoute.jsx
@@ -7,23 +7,23 @@ function ProtectedRoute() {
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (isLoggedIn) {
-      Swal.fire({
-        title: "로그인이 필요합니다!",
-        text: "코드 마에스트로의 회의 서비스는 로그인 후 이용 가능합니다.",
-        confirmButtonText: "확인",
-      }).then(() => {
-        // SweetAlert 확인 버튼 누른 뒤 이동
-        navigate("/login");
-      });
-    }
-  }, [isLoggedIn, navigate]);
+  // useEffect(() => {
+  //   if (!isLoggedIn) {
+  //     Swal.fire({
+  //       title: "로그인이 필요합니다!",
+  //       text: "코드 마에스트로의 회의 서비스는 로그인 후 이용 가능합니다.",
+  //       confirmButtonText: "확인",
+  //     }).then(() => {
+  //       // SweetAlert 확인 버튼 누른 뒤 이동
+  //       navigate("/login");
+  //     });
+  //   }
+  // }, [isLoggedIn, navigate]);
 
-  // 로그인 전이라면 자식 컴포넌트(=보호된 페이지)는 렌더링하지 않음
-  if (isLoggedIn) {
-    return null;
-  }
+  // // 로그인 전이라면 자식 컴포넌트(=보호된 페이지)는 렌더링하지 않음
+  // if (!isLoggedIn) {
+  //   return null;
+  // }
 
   // 로그인된 상태라면 그대로 children 렌더링
   return <Outlet />;
