2717e1fe85c137ceb09a66796cda13e1713a26d1
김태영 <ktylp0728@gmail.com>
2025-02-04 15:19:51 +0900

    [Fix] 예전에 작성한 활용하지 않는 user-kick 관련 함수 제거

--- backend/codemaestro/src/main/java/com/ssafy/codemaestro/domain/openvidu/controller/ConferenceManageController.java
+++ backend/codemaestro/src/main/java/com/ssafy/codemaestro/domain/openvidu/controller/ConferenceManageController.java
@@ -1,16 +1,8 @@
 package com.ssafy.codemaestro.domain.openvidu.controller;
 
-import com.ssafy.codemaestro.domain.auth.dto.CustomUserDetails;
-import com.ssafy.codemaestro.domain.openvidu.dto.ConferenceKickUserRequest;
 import com.ssafy.codemaestro.domain.openvidu.service.OpenViduService;
-import com.ssafy.codemaestro.domain.user.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,18 +13,5 @@ public class ConferenceManageController {
     @Autowired
     public ConferenceManageController(OpenViduService openViduService) {
         this.openViduService = openViduService;
-    }
-
-    @DeleteMapping("/user")
-    public ResponseEntity<Void> kickUser(@RequestBody ConferenceKickUserRequest dto, @AuthenticationPrincipal CustomUserDetails userDetails) {
-        User user = userDetails.getUser();
-
-        boolean result = openViduService.kickUserFromConference(user, dto.getConferenceId(), dto.getTargetUserId());
-
-        if (result) {
-            return new ResponseEntity<>(HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
     }
 }
