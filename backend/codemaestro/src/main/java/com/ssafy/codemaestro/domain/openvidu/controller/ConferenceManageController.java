90654dc9aa89fe0a6b58b862e7d49c0e4106df2b
김태영 <ktylp0728@gmail.com>
2025-02-04 16:06:49 +0900

    [Fix] user_conference 철자 오류 및 webScoketBroker의 allowEndpoint 임의로 테스트하던 것 정상화

--- backend/codemaestro/src/main/java/com/ssafy/codemaestro/domain/openvidu/controller/ConferenceManageController.java
+++ backend/codemaestro/src/main/java/com/ssafy/codemaestro/domain/openvidu/controller/ConferenceManageController.java
@@ -4,8 +4,9 @@ import com.ssafy.codemaestro.domain.openvidu.service.OpenViduService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/conference/manage")
 public class ConferenceManageController {
     OpenViduService openViduService;
