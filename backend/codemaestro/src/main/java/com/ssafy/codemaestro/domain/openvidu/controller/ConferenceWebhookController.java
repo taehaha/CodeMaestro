fcc9757db5151c72a1b9a4d094bca17d3833d3ab
Taeyeong Kim <ktylp0728@gmail.com>
2025-02-08 18:16:15 +0900

    [Refactor] OpenViduUtil.sendSignal 메서드 작성 및 OpenviduSignalType VO 작성 및 문법 OpenVidu -> Openvidu로 변경

--- backend/codemaestro/src/main/java/com/ssafy/codemaestro/domain/openvidu/controller/ConferenceWebhookController.java
+++ backend/codemaestro/src/main/java/com/ssafy/codemaestro/domain/openvidu/controller/ConferenceWebhookController.java
@@ -1,6 +1,7 @@
 package com.ssafy.codemaestro.domain.openvidu.controller;
 
-import com.ssafy.codemaestro.domain.openvidu.service.OpenViduWebHookService;
+import com.ssafy.codemaestro.domain.openvidu.service.OpenviduWebHookService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,18 +15,20 @@ import java.util.Map;
 /**
  * 여기서 접속 관련 DB 동기화를 실행하면 됨
  */
+@Slf4j
 @RestController
 @RequestMapping("/conference")
 public class ConferenceWebhookController {
-    OpenViduWebHookService openViduWebHookService;
+    OpenviduWebHookService openviduWebHookService;
 
     @Autowired
-    public ConferenceWebhookController(OpenViduWebHookService openViduWebHookService) {
-        this.openViduWebHookService = openViduWebHookService;
+    public ConferenceWebhookController(OpenviduWebHookService openviduWebHookService) {
+        this.openviduWebHookService = openviduWebHookService;
     }
 
     @PostMapping("/webhook")
     public ResponseEntity<Void> OpenViduWebhook(@RequestBody Map<String, Object> eventInfo) {
+        log.debug("OpenVidu Webhook Event triggered.");
         eventInfo.entrySet().forEach(System.out::println);
 
         String event = (String) eventInfo.get("event");
@@ -35,17 +38,17 @@ public class ConferenceWebhookController {
 
         } else if ("sessionDestroyed".equals(event)) {
             String sessionId = (String) eventInfo.get("sessionId");
-            openViduWebHookService.onSessionDestroyed(sessionId);
+            openviduWebHookService.onSessionDestroyed(sessionId);
 
         } else if ("participantJoined".equals(event)) {
             String sessionId = (String) eventInfo.get("sessionId");
             String connectionId = (String) eventInfo.get("connectionId");
             String serverData = (String) eventInfo.get("serverData");
-            openViduWebHookService.onParticipantJoined(serverData, sessionId, connectionId);
+            openviduWebHookService.onParticipantJoined(serverData, sessionId, connectionId);
 
         } else if ("participantLeft".equals(event)) {
             String connectionId = (String) eventInfo.get("connectionId");
-            openViduWebHookService.onParticipantLeft(connectionId);
+            openviduWebHookService.onParticipantLeft(connectionId);
         }
 
         return new ResponseEntity<>(HttpStatus.OK);
