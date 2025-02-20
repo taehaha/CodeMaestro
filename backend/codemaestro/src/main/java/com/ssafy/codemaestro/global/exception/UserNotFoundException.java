eef89835e7f5d4202935d30f1c470e6c8bbc3130
김태영 <ktylp0728@gmail.com>
2025-02-20 16:24:57 +0900

    [Fix] Backend 파일 제거

--- backend/codemaestro/src/main/java/com/ssafy/codemaestro/global/exception/UserNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.codemaestro.global.exception;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String message) { super(message); }
-}
--- backend/codemaestro/src/main/java/com/ssafy/codemaestro/global/exception/openvidu/CannotFindConnectionException.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.codemaestro.global.exception.openvidu;
-
-public class CannotFindConnectionException extends RuntimeException {
-    Long userId;
-    public CannotFindConnectionException(Long userId, String message) {
-        super(message);
-        this.userId = userId;
-    }
-}
