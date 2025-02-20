898f7af2b498ab600460678d7395f34440d3b4d7
김태영 <ktylp0728@gmail.com>
2025-02-20 10:19:17 +0900

    [Fix] CORS 오류 임시처방

--- frontend/codemaestro/packages/my-ide/server/server.js
+++ frontend/codemaestro/packages/my-ide/server/server.js
@@ -20,7 +20,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.REACT_APP_FRONTEND_URL,
+    // origin: process.env.REACT_APP_FRONTEND_URL,
+    origin: "https://www.codemaestro.site",
   })
 );
 app.use(express.json());
