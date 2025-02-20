168ea64b2fb6d21dd1d56e9ab8c7cc9c979653dc
Leejuyoung4 <leeju08335@gmail.com>
2025-02-20 11:25:29 +0900

    [DESIGN] 메인페이지 멘트 수정

--- frontend/codemaestro/packages/my-code/src/components/MainSection/MainSection.jsx
+++ frontend/codemaestro/packages/my-code/src/components/MainSection/MainSection.jsx
@@ -18,7 +18,7 @@ function MainSection() {
           우리 모두 함께해요, <br />
           <span className="highlight">코드 마에스트로</span>
         </h1>
-        <p>실시간 화상 회의, 동시 편집 코드 툴, AI 기반 코드 리뷰까지! 협업과 학습을 한 곳에서 경험하세요.</p>
+        <p>함께 보고, 함께 코딩하며 알고리즘 실력을 키우는 개발자들의 온라인 학습 공간</p>
         <div className={`buttons ${isVisible ? 'visible' : ''}`}>
         <Link to="/meeting">
           <button className="start-btn">무료로 시작하기</button>
