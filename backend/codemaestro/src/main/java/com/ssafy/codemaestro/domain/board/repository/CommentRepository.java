4461cf8badbb9b7191811c5e1392377c2ae1bcbc
Taeyeong Kim <ktylp0728@gmail.com>
2025-02-23 22:49:09 +0900

    [Fix] backend 파일 삭제

--- backend/codemaestro/src/main/java/com/ssafy/codemaestro/domain/board/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.codemaestro.domain.board.repository;
-
-import com.ssafy.codemaestro.global.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByBoardId(Long boardId);
-}
--- backend/codemaestro/src/main/java/com/ssafy/codemaestro/global/entity/LoginProvider.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.codemaestro.global.entity;
-
-public enum LoginProvider {
-    LOCAL,
-    KAKAO,
-    NAVER,
-    GOOGLE
-}
